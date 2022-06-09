1d0f8f04939131e45541f78b2d464b4c28aed148
Gonzalo <gonzalobarravalle@gmail.com>
2022-06-09 06:22:29 -0400

    deploy

---
+++ precache-manifest.be93fdc5f6f1222e59b1fe04c16495a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afea991488d7831c80e",
+    "url": "/login-app-firebase/css/app.13eca1ab.css"
+  },
+  {
+    "revision": "fb199026b142bd89de11",
+    "url": "/login-app-firebase/css/chunk-vendors.6e46c67e.css"
+  },
+  {
+    "revision": "f285324ef2199cd18f231fcc3ca66794",
+    "url": "/login-app-firebase/img/background.f285324e.png"
+  },
+  {
+    "revision": "96ae65e0e2ad95e8faa3b74feac334ff",
+    "url": "/login-app-firebase/index.html"
+  },
+  {
+    "revision": "9afea991488d7831c80e",
+    "url": "/login-app-firebase/js/app.937f5bee.js"
+  },
+  {
+    "revision": "fb199026b142bd89de11",
+    "url": "/login-app-firebase/js/chunk-vendors.7ef2b385.js"
+  },
+  {
+    "revision": "6b27bf339baa021128a3333f27d7046e",
+    "url": "/login-app-firebase/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/login-app-firebase/robots.txt"
+  }
+]);
