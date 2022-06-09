ad5f589fa8233259f979679cfb64a08bb531cb11
Gonzalo <gonzalobarravalle@gmail.com>
2022-06-09 02:45:26 -0400

    arreglo de librerias

--- src/main.js
+++ src/main.js
@@ -2,19 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import {firebaseApp} from './firebase/firebaseInit.js'
 import router from "./router";
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
-
-Vue.use(VueSweetalert2);
-
 import { BootstrapVue, IconsPlugin, BootstrapVueIcons  } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-
-// Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 Vue.use(BootstrapVueIcons)
 
